eb0876c42648a4d67ade7496139c07187dc1a0fb
RaySniper <61223047+RaySniper@users.noreply.github.com>
2020-02-19 18:57:56 +0800

    Update HelloWorld.java

--- HelloWorld/src/cn/HelloWorld.java
+++ HelloWorld/src/cn/HelloWorld.java
@@ -3,5 +3,9 @@ package cn;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,idea");
+    
+        
+    
     }
 }
